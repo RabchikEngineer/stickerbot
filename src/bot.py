9f78ea69369f26a5794da0777da80b1c51ac7eec
Rabchik2006 <rabcenco.valera@gmail.com>
2023-10-01 12:48:08 +0300

    add ability to update sticker pictures

--- src/bot.py
+++ src/bot.py
@@ -49,6 +49,19 @@ def filename_to_str(filename):
     return "\n".join(name_list)
 
 
+async def save_file(file, filename):
+    content_type = file.content_type.split('/')
+    temp_filename = directories['temp_directory'] + 'temp.' + content_type[1]
+    await file.save(temp_filename)
+    if content_type[0] == "image":
+        img = Image.open(temp_filename)
+        x, y = img.size
+        img.resize((160, int(160 * y / x)), Image.LANCZOS) \
+            .save(directories["stickers_dir"]+filename)
+        return 1
+    return 0
+
+
 @nice_output
 def refresh_stickerlist():
     global files
@@ -71,11 +84,6 @@ async def stickerlist(msg):
     await msg.channel.send(file=discord.File(directories["temp_directory"]+"stickerlist.png"))
 
 
-    # filenames = os.listdir('stickers')
-    # for filename in filenames:
-    #     await msg.channel.send(f'ID = {filename[:filename.find("#")]}\nНазвания:\n{filename_to_str(filename)[:-1]}',
-    #                            file=discord.File('stickers/'+filename))
-
 
 async def create_sticker(msg,args):
     if msg.attachments == []:
@@ -95,22 +103,11 @@ async def create_sticker(msg,args):
 
         return min(set(range(len(id_set)+2)).difference(id_set))
 
-    content_type=msg.attachments[0].content_type.split('/')
-    filename=directories['temp_directory']+'temp.'+content_type[1]
-    await msg.attachments[0].save(filename)
-    if content_type[0]=="image":
-        img=Image.open(filename)
-        x,y = img.size
-        img.resize((160,int(160*y/x)),Image.LANCZOS)\
-            .save(f'{directories["stickers_dir"]}{find_free_id()}#{sticker_names_to_filename(args)}.png')
-        await msg.channel.send(':white_check_mark:Стикер успешно добавлен:white_check_mark:')
-    elif content_type[0]=="video":
-        await msg.channel.send(developing_alert)
-        # video=Video(filename)
-        # video.resize(width=160)
-        # video.write_videofile(f'{directories["stickers_dir"]}{find_free_id()}#{sticker_names_to_filename(args)}.gif')
-    else:
+    filename=f'{find_free_id()}#{sticker_names_to_filename(args)}.png'
+    res=await save_file(msg.attachments[0], filename)
+    if not res:
         await msg.channel.send(':x:Неизвестный формат файла:x:')
+
     refresh_stickerlist()
 
 
@@ -122,20 +119,32 @@ async def delete_sticker(msg,args):
 
 
 async def update_sticker(msg,args):
-    filename = files.get(args[0])
-    names = set(sticker_filename_to_names(filename))
+    old_filename = files.get(args[0])
+    names = set(sticker_filename_to_names(old_filename))
 
-    if args[1]=="+":
-        names|=set(args[2:])
+    if len(args)>1:
+        if args[1]=="+":
+            names|=set(args[2:])
 
-    elif args[1]=="-":
-        names-=set(args[2:])
+        elif args[1]=="-":
+            names-=set(args[2:])
 
-    elif args[1] in ["x", "х"]:
-        names=set(args[2:])
+        elif args[1] in ["x", "х"]:
+            names=set(args[2:])
 
-    os.rename(directories["stickers_dir"]+filename,
-              f'{directories["stickers_dir"]}{filename.split("#")[0]}#{sticker_names_to_filename(names)}.png')
+    new_filename=f'{old_filename.split("#")[0]}#{sticker_names_to_filename(names)}.png'
+    print(old_filename)
+    print(new_filename)
+
+    if msg.attachments:
+        res = await save_file(msg.attachments[0], new_filename)
+        os.remove(directories["stickers_dir"]+old_filename)
+        res_dict = {0: ':x:Неизвестный формат файла:x:', 2: developing_alert}
+        if res in res_dict.keys():
+            await msg.channel.send(res_dict[res])
+            return
+    else:
+        os.rename(directories["stickers_dir"]+old_filename, directories["stickers_dir"]+new_filename)
 
     refresh_stickerlist()
     await msg.channel.send(':white_check_mark:Стикер успешно обновлён:white_check_mark:')
@@ -209,7 +218,7 @@ async def on_message(message):
     if channel.id==config["survey_channel_id"] and not message.author.bot:
         await create_survey(message, self_activation=True)
 
-    if (message_text.startswith('стикер') or message_text.startswith('с ')) and message_body in files:
+    if (message_text.startswith('стикер') or message_text.startswith('с ') or message_text.startswith('c ')) and message_body in files:
         # print(1)
 
         nick = message.author.nick or message.author.name
